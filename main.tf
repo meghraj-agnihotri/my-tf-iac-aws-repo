237c89486fb18e82307027d851e9ae30e3ca75ab
Meghraj Agnihotri <38850672+meghraj-agnihotri@users.noreply.github.com>
2022-09-11 19:21:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@ resource "aws_security_group" "my_security_group" {
   }
 
  ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
